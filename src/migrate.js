d17efc9e437c27bca88392d5a53fb8df91dd68df
Paul Scanlon <pauliescanlon@gmail.com>
2024-11-07 12:52:50 +0700

    feat: add migrate

---
+++ src/migrate.js
@@ -0,0 +1,21 @@
+import path from 'path';
+import { execSync } from 'child_process';
+import { fileURLToPath } from 'url';
+
+import { getConfigPaths } from './get-config-paths.js';
+
+try {
+  const configDir = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../configs');
+  const configPaths = getConfigPaths(configDir);
+
+  configPaths.forEach((configPath) => {
+    try {
+      console.log(`Running migrate for ${configPath}`);
+      execSync(`drizzle-kit migrate --config=${configPath}`, { encoding: 'utf-8' });
+    } catch (error) {
+      console.error(`Error generating with config ${configPath}:`, error);
+    }
+  });
+} catch (err) {
+  console.error('Error processing config directory:', err);
+}

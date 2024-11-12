6aab25dc69d3dbfbfbc8cc06980a677d836ed902
Paul Scanlon <pauliescanlon@gmail.com>
2024-11-12 14:13:16 +0700

    refactor: create automation scripts

--- configs/customer-3/drizzle.config.ts
+++ configs/customer-3/drizzle.config.ts
@@ -1,11 +1,11 @@
 import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
-
-export default defineConfig({
-  out: './drizzle/customer-3',
-  schema: './src/db/schema.ts',
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: process.env.DATABASE_URL_CUSTOMER_3!,
-  },
-});
+  import { defineConfig } from 'drizzle-kit';
+  
+  export default defineConfig({
+    out: './drizzle/customer-3',
+    schema: './src/db/schema.ts',
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: process.env.CUSTOMER_3!,
+    },
+  });

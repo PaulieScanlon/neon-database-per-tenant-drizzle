74707d20ac9922e647bd9e793eea4a5adeebafb9
Paul Scanlon <pauliescanlon@gmail.com>
2024-11-15 12:19:14 +0700

    refactor: update repoName and repoOwnder in generate script

--- src/scripts/generate.js
+++ src/scripts/generate.js
@@ -23,7 +23,7 @@ let secrets = [];
   }
 
   const { data: publicKeyData } = await octokit.request(
-    'GET /repos/PaulieScanlon/neon-database-per-tenant-drizzle/actions/secrets/public-key',
+    `GET /repos/${repoOwner}/${repoName}/actions/secrets/public-key`,
     {
       headers: {
         'X-GitHub-Api-Version': '2022-11-28',
@@ -32,62 +32,59 @@ let secrets = [];
   );
 
   const response = await neonApi.listProjects();
-  // const response = await neonApi.listProjects({ org_id: '' });
 
   const {
     data: { projects },
   } = await response;
 
   await Promise.all(
-    projects
-      .filter((project) => project.name !== 'paulie.dev') // TODO remove this
-      .map(async (project) => {
-        const { id, name } = project;
+    projects.map(async (project) => {
+      const { id, name } = project;
 
-        const response = await neonApi.getConnectionUri({
-          projectId: id,
-          database_name: 'neondb',
-          role_name: 'neondb_owner',
+      const response = await neonApi.getConnectionUri({
+        projectId: id,
+        database_name: 'neondb',
+        role_name: 'neondb_owner',
+      });
+
+      const {
+        data: { uri },
+      } = await response;
+
+      const safeName = name.replace(/\s+/g, '-').toLowerCase();
+      const path = `configs/${safeName}`;
+      const file = 'drizzle.config.ts';
+      const envVarName = `${safeName.replace(/-/g, '_').toUpperCase()}_DATABASE_URL`;
+      const encryptedValue = await encryptSecret(publicKeyData.key, uri);
+
+      secrets.push(envVarName);
+
+      if (!existsSync(path)) {
+        mkdirSync(path);
+        console.log('Set secret for :', safeName);
+      }
+
+      if (!existsSync(`${path}/${file}`)) {
+        writeFileSync(`${path}/${file}`, drizzleConfig(safeName, envVarName));
+        console.log('Create drizzle.config for:', safeName);
+      }
+
+      if (!existsSync(path) || !existsSync(`${path}/${file}`)) {
+        await octokit.request(`PUT /repos/${repoOwner}/${repoName}/actions/secrets/${envVarName}`, {
+          owner: repoOwner,
+          repo: repoName,
+          secret_name: envVarName,
+          encrypted_value: encryptedValue,
+          key_id: publicKeyData.key_id,
+          headers: {
+            'X-GitHub-Api-Version': '2022-11-28',
+          },
         });
+      }
 
-        const {
-          data: { uri },
-        } = await response;
-
-        const safeName = name.replace(/\s+/g, '-').toLowerCase();
-        const path = `configs/${safeName}`;
-        const file = 'drizzle.config.ts';
-        const envVarName = `${safeName.replace(/-/g, '_').toUpperCase()}_DATABASE_URL`;
-        const encryptedValue = await encryptSecret(publicKeyData.key, uri);
-
-        secrets.push(envVarName);
-
-        if (!existsSync(path)) {
-          mkdirSync(path);
-          console.log('Set secret for :', safeName);
-        }
-
-        if (!existsSync(`${path}/${file}`)) {
-          writeFileSync(`${path}/${file}`, drizzleConfig(safeName, envVarName));
-          console.log('Create drizzle.config for:', safeName);
-        }
-
-        if (!existsSync(path) || !existsSync(`${path}/${file}`)) {
-          await octokit.request(`PUT /repos/${repoOwner}/${repoName}/actions/secrets/${envVarName}`, {
-            owner: repoOwner,
-            repo: repoName,
-            secret_name: envVarName,
-            encrypted_value: encryptedValue,
-            key_id: publicKeyData.key_id,
-            headers: {
-              'X-GitHub-Api-Version': '2022-11-28',
-            },
-          });
-        }
-
-        execSync(`drizzle-kit generate --config=${path}/${file}`, { encoding: 'utf-8' });
-        console.log('Run drizzle-kit generate for :', safeName);
-      })
+      execSync(`drizzle-kit generate --config=${path}/${file}`, { encoding: 'utf-8' });
+      console.log('Run drizzle-kit generate for :', safeName);
+    })
   );
 
   const workflow = githubWorkflow(secrets);
